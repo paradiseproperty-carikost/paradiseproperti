4e324a914e025560c9fa83a6a56b9d8b7c5136c9
paradiseproperty-carikost <paradiseproperty.id@gmail.com>
2026-01-10 10:07:28 +0700

    Create kos.js

---
+++ data/kos.js
@@ -0,0 +1,14 @@
+const kosData = [
+  {
+    id: "kos-mawar",
+    nama: "Kos Putri Mawar",
+    lokasi: "Bandung",
+    harga: 800,
+    hargaText: "Rp 800.000 / bulan",
+    fasilitas: ["WiFi", "AC", "KM Dalam"],
+    deskripsi: "Kos nyaman dekat kampus dan minimarket.",
+    wa: "628123456789",
+    maps: "https://maps.google.com/?q=Bandung",
+    gambar: "assets/images/mawar.jpg"
+  }
+];
